14380cecd4a205c124f5e290bdf2b400c82c620d
Miran <mirankavinda12@gmail.com>
2023-11-04 14:48:03 +0530

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        topic: 'Computer Science',
+        question: 'What does CPU stand for?',
+        possibleAnswers: ['Central Processing Unit', 'Computer Program Unit', 'Central Power Unit'],
+        correctAnswer: 'Central Processing Unit',
+    },
+    {
+        topic: 'Software Engineering',
+        question: 'What is the purpose of version control systems in software development?',
+        possibleAnswers: ['To manage different versions of software', 'To write code faster', 'To improve user interface'],
+        correctAnswer: 'To manage different versions of software',
+    },
+    {
+        topic: 'Computer Science',
+        question: 'What is an algorithm?',
+        possibleAnswers: ['A step-by-step procedure for solving a problem', 'A type of computer hardware', 'A software bug'],
+        correctAnswer: 'A step-by-step procedure for solving a problem',
+    },
+    {
+        topic: 'Software Engineering',
+        question: 'What is the primary function of a compiler?',
+        possibleAnswers: ['To translate source code into machine code', 'To execute a program', 'To design user interfaces'],
+        correctAnswer: 'To translate source code into machine code',
+    },
+    {
+        topic: 'Computer Science',
+        question: 'What does HTML stand for in web development?',
+        possibleAnswers: ['Hypertext Markup Language', 'High-Technology Multilayer Language', 'Hyperlink Text Manipulation'],
+        correctAnswer: 'Hypertext Markup Language',
+    },
+];

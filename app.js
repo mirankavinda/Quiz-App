425e7b703d15c53ebb5a1b4ad6914625223133a4
Miran <mirankavinda12@gmail.com>
2023-11-06 12:25:00 +0530

    quize_progress

--- app.js
+++ app.js
@@ -30,3 +30,49 @@ const questions = [
         correctAnswer: 'Hypertext Markup Language',
     },
 ];
+
+const quizProgress = document.getElementById("quizProgress");
+const questionContainer = document.getElementById("questionContainer");
+const answerContainer = document.getElementById("answerContainer");
+let currentQuestionIndex = 0;
+
+function handleQuestion(index) {
+  // handle quiz progress section
+  quizProgress.innerHTML = "";
+  questions.forEach((question) => {
+    quizProgress.innerHTML += "<span></span>";
+  });
+  let spans = document.querySelectorAll("span");
+  for (let i = 0; i <= index; i++) {
+    spans[i].classList.add("seen");
+  }
+
+  // topic/question
+  questionContainer.innerHTML = `<p>${questions[index].topic}</p>
+  <p>${questions[index].question}</p>
+  `;
+
+  // answers
+  answerContainer.innerHTML = "";
+  questions[index].possibleAnswers.forEach((answer) => {
+    answerContainer.innerHTML += `<button>${answer}</button>`;
+  });
+  let answers = document.querySelectorAll("button");
+  answers.forEach((answer) => {
+    answer.addEventListener("click", (e) => {
+      if (e.target.textContent === questions[index].correctAnswer) {
+        console.log("correct! ");
+      } else {
+        console.log("wrong");
+      }
+      if (currentQuestionIndex === questions.length - 1) {
+        currentQuestionIndex = 0;
+      } else {
+        currentQuestionIndex++;
+      }
+      handleQuestion(currentQuestionIndex);
+    });
+  });
+}
+
+handleQuestion(currentQuestionIndex);
